cf6e24e5e60a886a5e71ff1204a98ba030f03d98
olivercodes <bryan@olivercodes.com>
2025-01-08 11:31:44 -0500

    fetch command with err handling

--- app.js
+++ app.js
@@ -5,11 +5,10 @@ import {
   InteractionResponseType,
   verifyKeyMiddleware,
 } from 'discord-interactions';
-import { getRandomEmoji, DiscordRequest } from './utils.js';
+import { DiscordRequest, getRoleByName, getMembersNoRole } from './utils.js';
+import { to } from 'await-to-js';
 
-// Create an express app
 const app = express();
-// Get port, or default to 3000
 const PORT = process.env.PORT || 3000;
 
 /**
@@ -19,6 +18,8 @@ const PORT = process.env.PORT || 3000;
 app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async function (req, res) {
   // Interaction type and data
   const { type, data, guild_id } = req.body;
+  const rolesUrl = `guilds/${guild_id}/roles`;
+  const membersUrl = `guilds/${guild_id}/members?limit=1000`;
 
   /**
    * Handle verification requests
@@ -32,46 +33,32 @@ app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async fun
    * See https://discord.com/developers/docs/interactions/application-commands#slash-commands
    */
   if (type === InteractionType.APPLICATION_COMMAND) {
+    let err, yardigan_role_id, members_with_no_role;
     const { name } = data;
+    const roleName = 'rolethatdoesntexist';
 
-    // "test" command - use this to verify you intalled the bot correctly
-    if (name === 'test') {
-      return res.send({
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: {
-          content: `hello world ${getRandomEmoji()}`,
-        },
-      });
-    }
-    
     if (name === 'lurkers') {
-      const rolesUrl = `guilds/${guild_id}/roles`;
-      const membersUrl = `guilds/${guild_id}/members?limit=1000`;
       try {
-        const rolesResponse = await DiscordRequest(rolesUrl, {
-          method: 'GET',
-        });
 
-        const roles = await rolesResponse.json();
-        const yardigan_role_id = roles
-          .filter(role => role.name === 'yardigans')
-          .map(role => role.id)
-          .join(", ");
+        [err, yardigan_role_id] = await to( getRoleByName(guild_id, roleName) ); 
+        if (err) {
+          return res.send({
+            type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+            data: {
+              content: `role ${roleName} not found`,
+            },
+          });
+        }
 
-        console.log(yardigan_role_id);
-
-        const membersResponse = await DiscordRequest(membersUrl, {
-          method: 'GET',
-        });
-
-        const members = await membersResponse.json();
-        console.log(members);
-        const members_with_no_role = members
-          .filter(member => member.roles.includes(yardigan_role_id) !== true)
-          .map(member => (member.user.username))
-          .join(", ");
-
-        console.log("members with no role", members_with_no_role);
+        [err, members_with_no_role] = await to( getMembersNoRole(guild_id, yardigan_role_id) );
+        if (err) {
+          return res.send({
+            type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+            data: {
+              content: `No members found matching the condition DOES_NOT_HAVE: ${roleName}`,
+            },
+          });
+        }
 
         return res.send({
           type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
@@ -79,20 +66,14 @@ app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async fun
             content: `Members that do not have the yardigans role: ${members_with_no_role}`,
           },
         });
-
-        console.log("members", members_with_no_role);
-
-
       } catch (err) {
         console.error('error with lurker command: ', err);
       }
     }
 
-    console.error(`unknown command: ${name}`);
     return res.status(400).json({ error: 'unknown command' });
   }
 
-  console.error('unknown interaction type', type);
   return res.status(400).json({ error: 'unknown interaction type' });
 });
 
--- utils.js
+++ utils.js
@@ -1,10 +1,19 @@
 import 'dotenv/config';
+import { to } from 'await-to-js';
+
+const DISCORD_GET = { method: 'GET' };
+
+export function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
 
 export async function DiscordRequest(endpoint, options) {
   // append endpoint to root API URL
   const url = 'https://discord.com/api/v10/' + endpoint;
+
   // Stringify payloads
   if (options.body) options.body = JSON.stringify(options.body);
+
   // Use fetch to make requests
   const res = await fetch(url, {
     headers: {
@@ -14,12 +23,14 @@ export async function DiscordRequest(endpoint, options) {
     },
     ...options
   });
+
   // throw API errors
   if (!res.ok) {
     const data = await res.json();
     console.log(res.status);
     throw new Error(JSON.stringify(data));
   }
+
   // return original response
   return res;
 }
@@ -36,12 +47,43 @@ export async function InstallGlobalCommands(appId, commands) {
   }
 }
 
-// Simple method that returns a random emoji from list
-export function getRandomEmoji() {
-  const emojiList = ['😭','😄','😌','🤓','😎','😤','🤖','😶‍🌫️','🌏','📸','💿','👋','🌊','✨'];
-  return emojiList[Math.floor(Math.random() * emojiList.length)];
+export async function getRoleByName(guildId, roleName) {
+  const rolesUrl = `guilds/${guildId}/roles`;
+
+  const [err, rolesResponse] = await to( DiscordRequest(rolesUrl, DISCORD_GET ));
+  if (err) {
+    return Promise.reject(`Failed to fetch roles from the discord server: ${rolesUrl}`);
+  }
+
+  const roles = await rolesResponse.json();
+  const roleId = roles
+    .filter(role => role.name === roleName)
+    .map(role => role.id)
+    .join(", ");
+
+  if (!roleId) {
+    return Promise.reject('role not found');
+  }
+
+  return roleId;
 }
 
-export function capitalize(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
+export async function getMembersNoRole(guild_id, role_id) {
+  const membersUrl = `guilds/${guild_id}/members?limit=1000`;
+  const [err, members] = await to( DiscordRequest(membersUrl, DISCORD_GET ));
+  if (err) {
+    return Promise.reject(`failed to fetch members from discord server: ${err}`);
+  }
+
+  const allMembers = await members.json();
+  const members_no_role = allMembers
+    .filter(member => member.roles.includes(role_id) !== true)
+    .map(member => (member.user.username))
+    .join(", ");
+
+  if (!members_no_role) {
+    return Promise.reject(`No members matching the condition were found.`);
+  }
+
+  return members_no_role;
 }
